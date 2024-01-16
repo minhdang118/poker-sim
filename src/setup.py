d601dec3e387e337702c785b8cff07ddb2c03f78
minhdang118 <mdang2@u.rochester.edu>
2024-01-15 22:52:57 -0500

    Update player actions

--- src/setup.py
+++ src/setup.py
@@ -79,27 +79,70 @@ class Player:
     def __init__(self, name):
         self.name = name
         self.hand = []
-        self.chips = 0
-        self.bet = 0
+        self.own_chips = 0
+        self.bet_chips = 0
         self.folded = False
         self.all_in = False
+        
+        # List of actions
+        # 0: Check
+        # 1: Bet
+        # 2: Call
+        # 3: Raise
+        # 4: Fold
+        # 5: All-in
+        self.actions_allowed = [False, False, False, False, False, False]
 
-    def add_chips(self, chips):
-        self.chips += chips
+    def action_check(self):
+        if self.actions_allowed[0]:
+            return True
+        else:
+            return False
     
-    def bet_chips(self, chips):
-        self.chips -= chips
-        self.bet += chips
+    def action_bet(self, bet_chips):
+        if self.actions_allowed[1]:
+            self.own_chips -= bet_chips
+            self.bet_chips += bet_chips
+            return True
+        else:
+            return False
+        
+    def action_call(self, call_chips):
+        if self.actions_allowed[2]:
+            self.own_chips -= call_chips
+            self.bet_chips += call_chips
+            return True
+        else:
+            return False
+        
+    def action_raise(self, raise_chips):
+        if self.actions_allowed[3]:
+            self.own_chips -= raise_chips
+            self.bet_chips += raise_chips
+            return True
+        else:
+            return False
+        
+    def action_fold(self):
+        if self.actions_allowed[4]:
+            self.hand = []
+            self.folded = True
+            return True
+        else:
+            return False
     
-    def fold(self):
-        self.folded = True
-    
-    def all_in(self):
-        self.all_in = True
+    def action_all_in(self):
+        if self.actions_allowed[5]:
+            self.own_chips = 0
+            self.bet_chips += self.own_chips
+            self.all_in = True
+            return True
+        else:
+            return False
     
     def reset(self):
         self.hand = []
-        self.bet = 0
+        self.bet_chips = 0
         self.folded = False
         self.all_in = False
 
@@ -122,18 +165,19 @@ class Game:
     def __init__(self):
         self.table = Table()
         self.num_players = self.table.players.length
+        self.players_in_game = self.table.players
         self.small_blind = 0
         self.big_blind = 0
         self.small_blind_position = 0
         self.big_blind_position = 0
         self.init_blinds()
 
-    def add_player(self, player_name):
-        self.table.players.append(player_name)
+    def add_player(self, player):
+        self.table.players.append(player)
         self.num_players += 1
     
-    def remove_player(self, player_name):
-        self.table.players.remove(player_name)
+    def remove_player(self, player):
+        self.table.players.remove(player)
         self.num_players -= 1
     
     def set_blinds(self, small_blind, big_blind):
@@ -149,7 +193,10 @@ class Game:
             player.hand.append(self.table.dealer.deal_hand())
             player.hand.append(self.table.dealer.deal_hand())
 
+    # def preflop_bet(self):
+    #     while True:
 
+            
 
     def deal_flop(self):
         self.table.dealer.burn()
